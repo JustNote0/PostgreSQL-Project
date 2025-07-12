07623dbe669cb688a1a6b1e6823aafb8a80cc394
Nurus Shobah Hidayati <nrshobahyt0.0@gmail.com>
2025-07-13 00:31:59 +0700

    data_menu.sql

---
+++ Insert_data/data_menu.sql
@@ -0,0 +1,11 @@
+INSERT INTO Menu (Menu_id,Restaurant_id, name, price, description) VALUES
+(50001, 40001, 'Sate Ayam', 25.000, 'Sate ayam dengan bumbu kacang'),
+(50002, 40002, 'Nasi Goreng Spesial', 35.000, 'Nasi goreng dengan telur, ayam, dan sayuran'),
+(50003, 40003, 'Ikan Bakar', 60.000, 'Ikan bakar dengan sambal pedas'),
+(50004, 40004, 'Bulgogi Daging Sapi', 79.000, 'Daging sapi yang diiris tipis dan dimarinasi dengan bumbu-bumbu pilihan.'),
+(50005, 40005, 'Nasi Padang Rendang', 40.000, 'Nasi padang dengan rendang daging sapi'),
+(50006, 40006, 'Xiao Long Bao', 55.000, 'Dimsum kukus berisi daging babi cincang dan kaldu yang meleleh di mulut.'),
+(50007, 40007, 'Nachos', 40.000, 'Keripik tortilla yang disajikan dengan keju leleh, daging cincang, jalapeno, dan saus.'),
+(50008, 40008, 'Chicken Tikka', 63.000, 'Potongan ayam yang dimarinasi dengan yogurt dan rempah-rempah.'),
+(50009, 40009, 'Quiche Lorraine', 75.000, 'Pai gurih yang diisi dengan campuran telur, krim, bacon, dan keju.'),
+(50010, 40010, 'Banh Mi', 33.000, 'Sandwich Vietnam yang diisi dengan daging, sayuran, dan saus khas Vietnam, disajikan dalam roti baguette.');
