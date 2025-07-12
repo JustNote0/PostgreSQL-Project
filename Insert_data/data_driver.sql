0eb9cc2e335b829efbb86f9279528cd426321f29
Nurus Shobah Hidayati <nrshobahyt0.0@gmail.com>
2025-07-13 00:32:17 +0700

    data_driver.sql

---
+++ Insert_data/data_driver.sql
@@ -0,0 +1,12 @@
+INSERT INTO Driver (Driver_id, name, phone, rating)
+VALUES
+(30001, 'Toni Saputra', 6281256789001, 4.5),
+(30002, 'Siti Nurhaliza', 6281367890123, 4.7),
+(30003, 'Tono Supriyanto', 6281478901234, 4.8),
+(30004, 'Joni Nugraha', 6281589012345, 4.6),
+(30005, 'Junedi Sari', 6281690123456, 4.9),
+(30006, 'Joko Widodo', 6281223456789, 4.4),
+(30007, 'Susi Susanti', 6281334567890, 4.2),
+(30008, 'Budi Santoso', 6281445678901, 4.5),
+(30009, 'Asep Pororo', 6281556789012, 4.3),
+(30010, 'Rina Astari', 6281667890123, 4.7);
