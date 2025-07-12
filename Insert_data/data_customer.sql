74258d69373bdad49a273bbf5ac6f79aa590da6a
Nurus Shobah Hidayati <nrshobahyt0.0@gmail.com>
2025-07-13 00:32:33 +0700

    data_customer.sql

---
+++ Insert_data/data_customer.sql
@@ -0,0 +1,12 @@
+INSERT INTO Customer (Customer_id, Name, Phone, Address)
+VALUES
+(20001, 'Ahmad Fauzi', 628123456789, 'Jl. Merdeka No. 1'),
+(20002, 'Mona Lisa', 628987654321, 'Jl. Kemerdekaan No. 2'),
+(20003, 'Ananda Sakinah', 628765432100, 'Jl. Sudirman No. 3'),
+(20004, 'Tutik Hidayah', 628112233445, 'Jl. Thamrin No. 4'),
+(20005, 'Budi Santoso', 628998877665, 'Jl. Ahmad Yani No. 5'),
+(20006, 'Joko Susanto', 628556677889, 'Jl. Kartini No. 6'),
+(20007, 'Tri Wahyudi', 628334455667, 'Jl. Panglima Sudirman No. 7'),
+(20008, 'Harry Poa', 628889900112, 'Jl. Dr. Soetomo No. 8'),
+(20009, 'Wawan Kurniawan', 628445566778, 'Jl. Diponegoro No. 9'),
+(20010, 'Dino Nugget', 628221133557, 'Jl. Bungtomo No. 10');
