97a1687a196e81089962a6047553443cd8ecf429
Nurus Shobah Hidayati <nrshobahyt0.0@gmail.com>
2025-07-13 00:30:35 +0700

    data_restaurant.sql

---
+++ Insert_data/data_restaurant.sql
@@ -0,0 +1,12 @@
+INSERT INTO Restaurant (Restaurant_id, name, location, cuisine_type, rating)
+VALUES
+(40001, 'Restoran Sate Ria', 'Surabaya', 'Indonesian', 4.7),
+(40002, 'Warung Nasi Goreng Jaya', 'Bandung', 'Indonesian', 4.5),
+(40003, 'Restoran Seafood Cirebon', 'Cirebon', 'Indonesian', 4.8),
+(40004, 'Bulgogi House', 'Surabaya', 'Korean', 4.6),
+(40005, 'Dapoer Nasi Padang', 'Medan', 'Indonesian', 4.9),
+(40006, 'Dim Sum King', 'Yogyakarta', 'Chinese', 4.4),
+(40007, 'La Cocina Mexicana', 'Bali', 'Mexican', 4.7),
+(40008, 'Tandoor Mahal', 'Makassar', 'Indian', 4.8),
+(40009, 'Café de Paris', 'Semarang', 'French', 4.3),
+(40010, 'Pho 88', 'Padang', 'Vietnamese', 4.6);
